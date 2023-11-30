0872a65fb5e37749d244417c930fcb63c56cb7b6
Santiago Linares <santilinares02@gmail.com>
2023-11-30 11:12:59 +0100

    Added a method to generate and store the questions. Another one for checking the answer to a given question. Then, a method to save the generated questions as a JSON file. And finally, a method to "read" the questions from the JSON and store them in a List.

--- src/main/java/org/santiagolinares/ChatGPTConfiguration.java
+++ src/main/java/org/santiagolinares/ChatGPTConfiguration.java
@@ -2,23 +2,28 @@ package org.santiagolinares;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatGPTConfiguration {
 
+    private static final String FILE_PATH = "./src/data/questions/questions.json";
     private static final String URL = "https://api.openai.com/v1/chat/completions";
     private static final String API_KEY = System.getenv("OPENAI_KEY"); // Get API key from environment variable
     private static final String MODEL = "gpt-3.5-turbo-1106";
-    private static final String system = "You are the presenter of a Quiz Game. You just make questions and check the " +
-            "correctness of the answers by saying yes or no";
-    public static String generateQuestions() {
 
-        String prompt = "Make a question. Without introducing yourself.";
+    private static String chatGPT(String prompt) {
 
         try {
             HttpClient httpClient = HttpClient.newBuilder().build();
@@ -30,7 +35,6 @@ public class ChatGPTConfiguration {
                     .POST(HttpRequest.BodyPublishers.ofString(
                       "{\"model\": \"" + MODEL + "\"," +
                             " \"messages\": [" +
-                              "{\"role\": \"system\", \"content\":  \"" + system + "\"}," +
                               "{\"role\": \"user\", \"content\": \"" + prompt + "\"}" +
                               "]}"))
                     .build();
@@ -53,10 +57,60 @@ public class ChatGPTConfiguration {
         }
     }
 
-    public String checkAnswer(String question, String answer){
-        String prompt = "";
-        return "";
+    public static void generateQuestions(){
+        String format = " The JSON file should follow a specific structure to store a list of quiz questions. " +
+                "Each question is represented as an individual set enclosed in curly braces. " +
+                "Inside each set, there is a key-value pair, where the key is question, " +
+                "and the associated value is the actual question. All these questions are organized as a list," +
+                " surrounded by square brackets. This list is also surrounded by curly braces. " +
+                "This format ensures that the JSON file is well-structured, " +
+                "making it easy to represent and organize multiple quiz questions, each " +
+                "identified by the question key. Give me this format in plain text";
+
+        String prompt = " Generate 10 questions for a quiz game. Format it for a JSON file." +
+                " Give me only the questions, without answers. " + format ;
+        String response = chatGPT(prompt);
+        System.out.println(response);
+        saveAsJson(response);
     }
+    public static String checkAnswer(String question, String answer){
+        String prompt = "Given this question: " + question + " Is this answer correct?: " + answer + ". " +
+                "Reply shortly with Yes or No.";
+        return chatGPT(prompt);
+    }
+
+    public static void saveAsJson(String jsonString){
+        try (FileWriter fileWriter = new FileWriter(FILE_PATH)) {
+            JSONObject jsonObject = new JSONObject(new JSONTokener(jsonString));
+            String formattedJsonString = jsonObject.toString(4);
+
+            fileWriter.write(formattedJsonString);
+            System.out.println("JSON file created successfully at: " + FILE_PATH);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static List<String> readQuestions(){
+        List<String> questions = new ArrayList<>();
+
+        try {
+            String content = new String(Files.readAllBytes(Paths.get(FILE_PATH)));
+            JSONObject jsonObject = new JSONObject(content);
+
+            JSONArray questionsArray = jsonObject.getJSONArray("questions");
+
+            for (int i = 0; i < questionsArray.length(); i++){
+                String question = questionsArray.getJSONObject(i).getString("question");
+                questions.add(question);
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return questions;
+    }
+
 
 
 }
